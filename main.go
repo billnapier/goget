e22d3f46708334eceebd70154940a623cee9dbd4
Bill Napier <napier@pobox.com>
2010-07-04 23:31:25 -0700

    Re-work the channels in main.go so all goroutines use the same channel to return the results.

--- main.go
+++ main.go
@@ -10,42 +10,38 @@ import (
 var username = flag.String("username", "", "Username for auth.")
 var password = flag.String("password", "", "Password for auth.")
 
-func GoGet(url string) chan string {
-	ch := make(chan string)
-	go func() {
-		outfile, err := goget.GetOutfile(url)
-		if err != nil {
-			fmt.Println("Error determining outfile: " + err.String())
-			os.Exit(1)
-		}	
+func GoGet(url string, resultChan chan string) {
+	outfile, err := goget.GetOutfile(url)
+	if err != nil {
+		fmt.Println("Error determining outfile: " + err.String())
+		os.Exit(1)
+	}	
 
-		authUrl, err := goget.BuildAuthUrl(url, *username, *password)
-		if err != nil {
-			fmt.Println("Error building auth url: " + err.String())
-			os.Exit(1)
-		}
-		err = goget.FetchUrl(authUrl, outfile)
-		if err != nil {
-			fmt.Println("Error fetching url: " + err.String())
-			os.Exit(1)
-		}
-		ch <- outfile
-	}()
-	return ch
+	authUrl, err := goget.BuildAuthUrl(url, *username, *password)
+	if err != nil {
+		fmt.Println("Error building auth url: " + err.String())
+		os.Exit(1)
+	}
+	err = goget.FetchUrl(authUrl, outfile)
+	if err != nil {
+		fmt.Println("Error fetching url: " + err.String())
+		os.Exit(1)
+	}
+	resultChan <- outfile
 }
 
 func main() {
 	flag.Parse()
 
-	results := make([]chan string, flag.NArg())
+	results := make(chan string)
 
 	// Kick off all the async downloads
 	for i := 0; i < flag.NArg(); i++ {
-		results[i] = GoGet(flag.Arg(i))
+		go GoGet(flag.Arg(i), results)
 	}
 
 	// And wait for their completion
 	for i := 0; i < flag.NArg(); i++ {
-		fmt.Println("Downloaded to " + <- results[i])
+		fmt.Println("Downloaded to " + <- results)
 	}
 }
